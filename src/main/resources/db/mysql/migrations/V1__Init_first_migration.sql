1789cd32de6dda6a2b6e114df23fc408e558eab8
mlevitan <maximlevitan@gmail.com>
2018-09-24 17:13:47 +0300

    Fix mysql character encoding settings

--- src/main/resources/db/mysql/migrations/V1__Init_first_migration.sql
+++ src/main/resources/db/mysql/migrations/V1__Init_first_migration.sql
@@ -1,3 +1,8 @@
+ALTER DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+SET collation_connection     = utf8_general_ci;
+SET character_set_results    = utf8;
+SET character_set_connection = utf8;
+SET character_set_client     = utf8;
 
 CREATE TABLE IF NOT EXISTS `app_user` (
   `id` varchar(255) NOT NULL,
